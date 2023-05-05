38f0da3d925f58c095049b4fcdbe6e6d7b94ab09
stanislavi18061989 <96617178+stanislavbukaevsky@users.noreply.github.com>
2023-05-05 20:04:29 +0300

    Методы для сервисов Ads, Comment and Image. Добавление методов для мапперов

--- src/main/java/pro/sky/diploma/constants/ExceptionTextMessageConstant.java
+++ src/main/java/pro/sky/diploma/constants/ExceptionTextMessageConstant.java
@@ -6,5 +6,8 @@ package pro.sky.diploma.constants;
 public class ExceptionTextMessageConstant {
     public static final String USER_NOT_FOUND_EXCEPTION = "Пользователя с таким именем не существует!";
     public static final String USER_NOT_FOUND_EXCEPTION_2 = "Такого пользователя не существует!";
+    public static final String USER_NAME_NOT_FOUND_EXCEPTION = "Пользователя с таким логином не существует!";
     public static final String COMMENT_NOT_FOUND_EXCEPTION = "Комментария с таким идентификатором не существует!";
+    public static final String COMMENT_AND_ADS_NOT_FOUND_EXCEPTION = "Комментария с таким идентификатором у этого объявления не существует!";
+    public static final String ADS_NOT_FOUND_EXCEPTION = "Объявления с таким идентификатором не существует!";
 }
